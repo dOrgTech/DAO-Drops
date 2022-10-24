9820871103189b68abe3ebed5dab23323610d97a
stan36 <chris@dorg.tech>
2022-10-24 11:59:04 -0700

    added express level sanitizastion, still needs testing. will test after lunch

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import body from "express-validator";
+import check from "express-validator";
 
 import {
   getPosts,
@@ -14,9 +14,66 @@ const router = express.Router();
 //reached at: localhost:5000/posts
 
 router.get("/", getPosts);
-router.post("/", createPost);
 router.get("/score/:id", getScore);
-router.patch("/score/:id", updateScore);
 router.get("/picks/", getPicks);
+router.post(
+  "/",
+  [
+    check("name")
+      .not()
+      .isEmpty()
+      .isLength({ min: 5 })
+      .trim()
+      .escape()
+      .withMessage("Name must have more than 5 characters"),
+    check("message", "Message should be a string")
+      .not()
+      .isEmpty(),
+    check("address", "address left blank")
+      .optional()
+      .trim()
+      .escape(),
+    check("link", "link left blank")
+      .optional()
+      .trim()
+      .escape(),
+    check("contact", "contact left blank").optional(),
+    check("contactMethod", "contactMethod left blank")
+      .optional()
+      .trim()
+      .escape(),
+    check("image", "image left blank")
+      .optional()
+      .trim()
+      .escape()
+  ],
+  createPost
+);
+router.patch(
+  "/score/:id",
+  [
+    check("account")
+      .not()
+      .isEmpty()
+      .isLength({ min: 5 })
+      .trim()
+      .escape()
+      .withMessage("Name must have more than 5 characters"),
+    check("score", "address left blank")
+      .isNumeric()
+      .not()
+      .isEmpty()
+      .trim()
+      .escape(),
+    check("picks", "picks cannot be left blank")
+      .isArray()
+      .not()
+      .isEmpty(),
+    check("message", "Message should be a signed token")
+      .not()
+      .isEmpty()
+  ],
+  updateScore
+);
 
 export default router;
