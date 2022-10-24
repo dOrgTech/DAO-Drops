2ee7db05ed2232b5cc96a9369b6b0e23fcdbf46a
Stan36 <stan36@pop-os.localdomain>
2022-10-24 17:44:21 +0000

    added rate limit for ddos protection, helmet for header protection and compression

--- server/index.js
+++ server/index.js
@@ -3,15 +3,33 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import * as dotenv from "dotenv";
+import helmet from "helmet";
+import compression from "compression";
+import rateLimit from "express-rate-limit";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
 dotenv.config();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+});
+
+// const isProduction = process.env.NODE_ENV === "production";
+// const origin = {
+//   origin: isProduction ? "https://www.daodrops.io" : "*"
+// };
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(limiter);
 
 app.use("/posts", postRoutes);
 
