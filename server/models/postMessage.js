66e068d7ebafe04f1a0e1a9a7f39b3415fa02364
stan36 <chris@dorg.tech>
2023-01-12 10:27:12 -0800

    removed getPosts route for phase one

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-
-const postSchema = mongoose.Schema({
-  name: String,
-  message: String,
-  address: String,
-  link: String,
-  contact: String,
-  contactMethod: String,
-  image: String,
-  createdAt: {
-    type: Date,
-    default: new Date()
-  }
-});
-
-const PostMessage = mongoose.model("PostMessage", postSchema);
-
-export default PostMessage;
+// import mongoose from "mongoose";
+//
+// const postSchema = mongoose.Schema({
+//   name: String,
+//   message: String,
+//   address: String,
+//   link: String,
+//   contact: String,
+//   contactMethod: String,
+//   image: String,
+//   createdAt: {
+//     type: Date,
+//     default: new Date()
+//   }
+// });
+//
+// const PostMessage = mongoose.model("PostMessage", postSchema);
+//
+// export default PostMessage;
